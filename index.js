cbb9650527e036f1ef1ffb62d1af5336f20f8192
DhrutiC7 <dhruti.chandarana@eronkan.com>
2021-01-11 12:13:33 +0530

    modifying index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-// index
-console.log('Hello World!');
+/**
+ * A demonstration on how to executes functions defined in different modules
+ */
+
+const function1 = require('./ch_1/example1');
+const function2 = require('./ch_1/example2');
+
+// executing functions according to the definition
+function1();                // immediate call
+function2('Hey there!');    // function with arguments
